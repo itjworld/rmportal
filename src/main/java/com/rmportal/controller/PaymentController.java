86f9e053f48ca12c173794da2a83a157f2406edc
anil.kundu <anil.kundu@GMAIL.COM>
2019-01-23 23:09:20 +0530

    CHANGES IMPLEMENTED FOR MONTH RECORDS

--- src/main/java/com/rmportal/controller/PaymentController.java
+++ src/main/java/com/rmportal/controller/PaymentController.java
@@ -3,22 +3,26 @@ package com.rmportal.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.rmportal.service.PaymentService;
 
-@RestController("/api/v1")
+@RestController
+@RequestMapping(value = "/api/v1")
 public class PaymentController {
-	
+
 	@Autowired
 	private PaymentService paymentService;
 
-	@PostMapping("/insert/records")
-	public ResponseEntity<String> insertRentRecords() {
+	@GetMapping("/month/records/insert")
+	public ResponseEntity<Void> insertRentRecords() {
 		boolean result = paymentService.insertMonthRecords();
-		return new ResponseEntity<String>("Records inserted successfully", HttpStatus.OK);
-
+		if (result)
+			return new ResponseEntity<Void>(HttpStatus.OK);
+		else
+			return new ResponseEntity<Void>(HttpStatus.INTERNAL_SERVER_ERROR);
 	}
 
 }
